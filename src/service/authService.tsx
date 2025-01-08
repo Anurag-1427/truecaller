20e91c2cc0fea1490ef670c68e822716b40d3369
Anurag Sharma <kaushikanurag2001@gmail.com>
2025-01-08 21:10:16 +0530

    feat: login and signup api calling

---
+++ src/service/authService.tsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { storage } from "../state/storage";
+import { useUserStore } from "../state/userStore";
+import { BASE_URL } from "./config";
+
+export const login = async (phoneNumber: any) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/login`, { phoneNumber });
+        const { accessToken, refreshToken, user } = response.data;
+        storage.set('accessToken', accessToken);
+        storage.set('refreshToken', refreshToken);
+        const { setUser } = useUserStore.getState();
+        setUser(user);
+        return response.data;
+
+    } catch (error) {
+        console.error('Login Error', error);
+        throw error;
+    }
+}
+
+export const signup = async (phoneNumber: any, email: string, name: string) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/signup`, { phoneNumber, email, name });
+        const { accessToken, refreshToken, user } = response.data;
+        storage.set('accessToken', accessToken);
+        storage.set('refreshToken', refreshToken);
+        const { setUser } = useUserStore.getState();
+        setUser(user);
+        return response.data;
+
+    } catch (error) {
+        console.error('Signup Error', error);
+        throw error;
+    }
+}
