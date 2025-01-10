b83a5def7cebd2b3e533c90abd398c81c113caa0
Anurag Sharma <kaushikanurag2001@gmail.com>
2025-01-10 20:51:50 +0530

    changes updated

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navigation from './src/navigation/Navigation'
+import { NativeModules } from 'react-native'
+import { checkPermissions } from './src/utils/PhoneService';
+
+const { CallScreeningModule } = NativeModules;
 
 const App = () => {
+
+  useEffect(() => {
+    checkPermissions(CallScreeningModule);
+  }, [])
+
   return (
     <Navigation />
   )
