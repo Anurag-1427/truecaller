4a792484e44f2e5a234922beb17ca02759670631
Anurag Sharma <kaushikanurag2001@gmail.com>
2025-01-13 21:24:34 +0530

    feat: Headless JS added

--- index.js
+++ index.js
@@ -5,5 +5,42 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import notifee from '@notifee/react-native';
+import {findUser} from './src/service/authService';
+import {storage} from './src/state/storage';
+
+notifee.onForegroundEvent(({type, detail}) => {});
+notifee.onBackgroundEvent(async ({type, detail}) => {});
+
+async function onDisplayNotification(user) {
+  await notifee.requestPermission();
+  const channelId = await notifee.createChannel({
+    id: 'default',
+    name: 'Default Channel',
+  });
+  await notifee.displayNotification({
+    title: `Call From ${user?.name ?? 'Unknown'}`,
+    body: `${user?.phoneNumber}`,
+    android: {
+      channelId,
+    },
+  });
+}
+
+const handleIncomingCall = async data => {
+  console.log(`data in handleIncomingCall: `, data);
+  const accessToken = storage.getString('accessToken');
+  if (accessToken) {
+    const phoneNumber = data?.phoneNumber;
+    const cleanedNumber = phoneNumber.replace(/[^\d]/g, '');
+    const last10Digits = cleanedNumber.slice(-10);
+    const user = await findUser(last10Digits);
+    onDisplayNotification(user);
+  }
+};
 
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerHeadlessTask(
+  'CallBackgroundMessaging',
+  () => handleIncomingCall,
+);
